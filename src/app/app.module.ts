9974420331052818d1e255ebd313278e47229840
monalisorathiya <monalisorathiya8@gmail.com>
2020-09-10 20:53:14 +0530

    Components in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MyContainerComponent } from './my-container/my-container.component';
 import { TopNavComponent } from './my-container/top-nav/top-nav.component';
+import { HeaderComponent } from './my-container/header/header.component';
+import { Textsection1Component } from './my-container/textsection1/textsection1.component';
+import { Textsection2Component } from './my-container/textsection2/textsection2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyContainerComponent,
-    TopNavComponent
+    TopNavComponent,
+    HeaderComponent,
+    Textsection1Component,
+    Textsection2Component
   ],
   imports: [
     BrowserModule
