0cd75c91c597651be1def4af540ca98d4fab802d
monalisorathiya <monalisorathiya8@gmail.com>
2020-09-10 11:26:44 +0530

    components in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyContainerComponent } from './my-container/my-container.component';
+import { TopNavComponent } from './my-container/top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyContainerComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule
